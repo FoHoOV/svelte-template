b3e60173e0b9662fa6db5c39e28f6a1e1ccbb3d5
godzylinux <fohoov@gmail.com>
2023-08-14 21:13:35 +0330

    added reactive if to protect the routes

--- src/routes/user/+layout.svelte
+++ src/routes/user/+layout.svelte
@@ -5,13 +5,13 @@
 	import { isRouteProtected } from './protected-routes';
 	import { browser } from '$app/environment';
 
-	if (browser && isRouteProtected($page.route.id ?? '') && !$user?.access_token) {
+	$: if (browser && isRouteProtected($page.route.id ?? '') && !$user?.access_token) {
 		console.log('ran layout check');
 		console.log($page.route.id);
 		goto('/user/login');
 		console.log($navigating);
 	}
-	
+
 	console.log('ran layout');
 </script>
 
