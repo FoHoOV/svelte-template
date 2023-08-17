f5348b92202b28cb4c48b7017dae73c26defa21f
godzylinux <fohoov@gmail.com>
2023-08-17 22:44:00 +0330

    implemented error handling for logout form

--- src/routes/user/logout/+page.svelte
+++ src/routes/user/logout/+page.svelte
@@ -1,25 +1,34 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import LoadingButton from '$lib/components/buttons/LoadingButton.svelte';
-	import type { ActionData } from './$types';
+	import FormError from '../../../lib/components/forms/FormError.svelte';
+	import type { ActionData, SubmitFunction } from './$types';
 
 	export let form: ActionData;
+
+	let errorTitle: string | undefined;
 
 	let isSubmitting = false;
 	const handleSubmit = () => {
 		isSubmitting = true;
 	};
-	const handleError = (error: Event) => {
+	const handleError: SubmitFunction = () => {
 		isSubmitting = false;
-		console.log(error);
-		console.log(form);
-		debugger;
+		return async ({ result, update }) => {
+			if (result.type == 'error') {
+				isSubmitting = false;
+				errorTitle = result.error.message;
+				return;
+			}
+			await update();
+		};
 	};
 </script>
 
 <div class="flex flex-col justify-center items-center">
+	<FormError error={errorTitle} />
 	<h2>Are sure you are gonna miss out on this?</h2>
-	<form method="post" use:enhance on:submit={handleSubmit} on:error={handleError}>
+	<form method="post" use:enhance={handleError} on:submit={handleSubmit}>
 		<LoadingButton
 			className="btn-warning mt-2"
 			text="Yes, I'm a piece of shit"
