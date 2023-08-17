5be2d0e230a5fe4924e098983d7b39e327274cef
godzylinux <fohoov@gmail.com>
2023-08-17 22:30:08 +0330

    we are sure that routeid is not null because it is client-side only

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,7 @@
 
 	beforeNavigate(async ({ to, cancel }) => {
 		console.log(to);
-		if (browser && isRouteProtected(to?.route.id ?? '/') && !$page.data.token) {
+		if (browser && isRouteProtected(to?.route.id!) && !$page.data.token) {
 			// because if the client-side router kicks in and page does NOT
 			// have a page.server.ts which doesn't fire the hooks.server.ts then
 			// the an unauthenticated user might see an authenticated page
