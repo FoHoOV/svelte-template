484de3a0eaacede871bbc417d9ff0e4bfda0deea
godzylinux <fohoov@gmail.com>
2023-08-24 00:37:29 +0330

    added skeletonUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
-import daisyui from 'daisyui';
-import typography from '@tailwindcss/typography';
-
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+import path from 'path'
+import skeleton from "@skeletonlabs/skeleton/tailwind/skeleton"
+module.exports = {
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		path.join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
 	theme: {
-		extend: {}
+		extend: {},
 	},
-	plugins: [typography, daisyui]
-};
+	plugins: [
+		...skeleton()
+	]
+}
